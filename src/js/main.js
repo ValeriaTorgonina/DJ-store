ea436fec7462740560f5a3fe3f9905239d11a5b6
Valeria Torgonina <lovec.cnov@mail.ru>
2020-06-25 18:14:38 +0300

    create form popup

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,63 @@ class Header {
     }
 };
 
+class Form {
+    constructor(
+        form = document.querySelector('.form'),
+        formSuccess = document.querySelector('.form-success')
+    ) {
+        this.form = form;
+        this.formSuccess = formSuccess;
+        this.addHandlersForForm();
+    }
+
+    addHandlersForForm() {
+        this.form.addEventListener('submit', e => {
+            e.preventDefault();
+            this.form.hidden = true;
+            this.formSuccess.hidden = false;
+        })
+    }
+}
+
+class Popup {
+    constructor(
+        closeBtns = [...document.querySelectorAll('.cross-btn')]
+    ) {
+        this.overlay = document.querySelector('.overlay');
+        this.closeBtns = closeBtns;
+        this.addHandlersForCloseBtns();
+    }
+
+    addHandlersForCloseBtns() {
+        document.body.addEventListener('click', e => {
+            let target = e.target;
+            let currentTarget = e.currentTarget;
+            let isCloseBtn = false;
+            while(target !== currentTarget) {
+                if(this.closeBtns.includes(target)) {
+                    isCloseBtn = true;
+                }
+                if(isCloseBtn && target.classList.contains('popup')) {
+                    return this.closePopup(target);
+                }
+
+                target = target.parentElement;
+            }
+        })
+    }
+
+    closePopup(popup) {
+        popup.hidden = true;
+        this.overlay.hidden = true;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    var header = new Header();
+    const header = new Header();
+    const form = new Form();
+    const popup = new Popup([
+        ...document.querySelectorAll('.cross-btn'),
+        form.formSuccess.querySelector('.blue-btn')
+    ]);
 });
